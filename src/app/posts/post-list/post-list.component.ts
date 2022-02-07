51a830e961951c2bdbbb1bc5d01fdf0e148226f8
Yusuf-Samodien <yusufsamodien5@gmail.com>
2022-02-07 17:14:04 +0200

    Some optimizations, added a posts.service

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -1,16 +1,33 @@
-import { Component, Input } from "@angular/core";
+import { Component,  OnDestroy,  OnInit } from "@angular/core";
+import { Subscription } from "rxjs";
+
+import {Post} from '../post.model';
+import { PostsService } from "../posts.service";
 
 @Component({
   selector: 'app-post-list',
   templateUrl: './post-list.component.html',
   styleUrls: ['./post-list.component.css']
 })
-export class PostListComponent{
+export class PostListComponent implements OnInit, OnDestroy{
   //posts = [
   //  {title: "First Post", content: "This is the first posts\'s content"},
    // {title: "Second Post", content: "This is the second posts\'s content"},
     //{title: "Third Post", content: "This is the third posts\'s content"},
   //];
 
-  @Input() posts = [];
+  posts: Post[] = [];
+  private postsSub: Subscription;
+
+  constructor(public postsService: PostsService){}
+  ngOnInit(){
+    this.posts=this.postsService.getPosts();
+    this.postsSub = this.postsService.getPostUpdateListener().subscribe((posts: Post[]) => {this.posts=posts;})
+  }
+
+  ngOnDestroy(): void {
+      this.postsSub.unsubscribe;
+  }
 }
+
+
